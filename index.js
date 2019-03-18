61cb47d583e24b5bcfd1c0abda9f414eb49289e6
bb <bbonneville+node@hexaglobe.com>
2019-03-18 14:35:31 +0100

    Create load products function/ add getAllProducts to display all products

--- index.js
+++ index.js
@@ -2,39 +2,54 @@ var products = require('./products.json');
 
 var fs = require('fs');
 
+function loadProducts(callback) {
 
-fs.open("./products.json",'r',(err,fd) => {
-    fs.fstat(fd, function(err, stats) {
-        
-        if(err) {
-            console.log(err);
-            return;
-        }
-
-        var buffer = new Buffer(stats.size);
-        
-        fs.read(fd,buffer,0,stats.size,null,(err,byteReads, buffer) => {
-        
+    fs.open("./products.json",'r',(err,fd) => {
+        fs.fstat(fd, function(err, stats) {
+            
             if(err) {
                 console.log(err);
                 return;
             }
-            var json = buffer.toString();
 
-            try {
-                var products = JSON.parse(json);
-            } catch(e) {
-                console.log(e);
-                return;
-            }
+            var buffer = new Buffer(stats.size);
+            
+            fs.read(fd,buffer,0,stats.size,null,(err,byteReads, buffer) => {
+            
+                if(err) {
+                    console.log(err);
+                    return;
+                }
+                var json = buffer.toString();
 
-            console.log(products[1]);
+                try {
+                    var products = JSON.parse(json);
+                } catch(e) {
+                    console.log(e);
+                    return;
+                }
+
+                callback(products);
+            
+            });
+        });
+
         
+    })
+
+}
+
+function getAllProducts() {
+    console.log("Bienvenue. Voici les produits disponibles :");
+    loadProducts((products) => {
+        products.forEach(function(product) {
+            console.log(`${product.id} - ${product.name} / ${product.EUR_price} / ${product.orders_counter}`);
         });
     });
+}
 
-    
-})
+getAllProducts();
+
 /*
 fs.readFile("./products.json",(err,data) => {
 
